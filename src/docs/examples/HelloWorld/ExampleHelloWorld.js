c25f0971de222d4cfc4321ed8e17d31a40d62994
DanBonehill <DanBonehill@me.com>
2018-12-06 15:22:05 +0000

    Complete Documentation Generation

---
+++ src/docs/examples/HelloWorld/ExampleHelloWorld.js
@@ -0,0 +1,7 @@
+import React from "react";
+import HelloWorld from "ps-react/HelloWorld";
+
+/** Basic Example */
+const ExampleHelloWorld = () => <HelloWorld message="Pluralsight viewers!" />;
+
+export default ExampleHelloWorld;
