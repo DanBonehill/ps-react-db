7d1ff8f52f992fee471bfa6b3ceae9541750d808
DanBonehill <DanBonehill@me.com>
2018-12-06 18:37:22 +0000

    Create Atoms

---
+++ src/docs/examples/ProgressBar/Example100Percent.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ProgressBar from "ps-react/ProgressBar";
+
+/** 100% progress */
+const Example100Percent = () => <ProgressBar percent={100} width={150} />;
+
+export default Example100Percent;
